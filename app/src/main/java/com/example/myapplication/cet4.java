330b1f53805928fb3db7ed0ce7af58f777346739
10520147@QQ.com <Zhang.12345678>
2020-10-20 20:30:08 +0800

    通过内网解析实现初步背单词功能

--- app/src/main/java/com/example/myapplication/cet4.java
+++ app/src/main/java/com/example/myapplication/cet4.java
@@ -11,14 +11,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.SplittableRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.util.Random;
 //1.0.2
 public class cet4 extends AppCompatActivity {
     Button cet4;
     TextView textView;
+    String re;
     String fileName="cet4";
     private int count=0;
 
@@ -38,32 +56,155 @@ public class cet4 extends AppCompatActivity {
     }
 
 
-    public void btn2Click(View view) throws IOException {
 
-        count++;
+    public void btn1Click(View view) throws IOException {
 
-        FileInputStream fileInputStream = openFileInput(fileName);
-        byte[] input = new byte[fileInputStream.available()];
-        while(fileInputStream.read(input) != -1){
-            String str = new String(input);
-            String [] s = str.split("##");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-            if(count%2==0){
 
-                textView.setText("英文：" + s[count] + "   中文：" + s[count + 1]);
-            }else {
+                    requestDate();
 
-                textView.setText("英文：" + s[count+1]);
-                //textView.setText("英文：" + s[0] + "   中文：" + s[1]);
+
             }
-        }
+        }).start();
+    }
 
 
-        if(fileInputStream !=null){
-            fileInputStream.close();
+
+
+
+
+
+
+
+    private void requestDate(){
+       // textView.setText("sb");
+
+
+        Random rand = new Random();
+
+
+        int random=rand.nextInt(5);
+
+
+
+
+        URL url = null;
+        try {
+            url = new URL("http://mrqd8dcc.shenzhuo.vip:11532/id?id="+random);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)//访问连接
+                .get()
+                .build();
+        Call call = client.newCall(request);
+
+
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                //
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("sb");
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                re = response.body().string();
+//                //textView.setText("sb");
+//
+//                String chinese = "chinese\":(.+?),";
+//                Matcher ch = Pattern.compile(chinese).matcher(re);
+//
+//                while (ch.find()){
+//                    String chi = ch.group(1);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(re);
+
+                        }
+                    });
+
+
+
+                //}
+                //textView.setText(re);
+//                textView.setText(re);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //以下是内存储模式下的单词读取
+//    public void btn2Click(View view) throws IOException {
+//
+//        count++;
+//
+//        FileInputStream fileInputStream = openFileInput(fileName);
+//        byte[] input = new byte[fileInputStream.available()];
+//        while(fileInputStream.read(input) != -1){
+//            String str = new String(input);
+//            String [] s = str.split("##");
+//
+//            if(count%2==0){
+//
+//                textView.setText("英文：" + s[count] + "   中文：" + s[count + 1]);
+//            }else {
+//
+//                textView.setText("英文：" + s[count+1]);
+//                //textView.setText("英文：" + s[0] + "   中文：" + s[1]);
+//            }
+//        }
+//
+//
+//        if(fileInputStream !=null){
+//            fileInputStream.close();
+//        }
+//
+//    }
 
 
 
