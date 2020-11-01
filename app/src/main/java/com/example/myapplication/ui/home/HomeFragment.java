4517d0d5a455f4afe17149393f84c236fa9fdb32
10520147@QQ.com <Zhang.12345678>
2020-11-01 19:00:12 +0800

    实现简易轮播图

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,17 +1,12 @@
 package com.example.myapplication.ui.home;
+import androidx.fragment.app.Fragment;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,36 +14,30 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.cet4;
-import com.example.myapplication.fanyi;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class HomeFragment extends Fragment {
+    ViewPager mViewpage;
+    LinearLayout mLayout;
+    ArrayList<ImageView> dots = new ArrayList<>();
+    ArrayList<Integer> imgs = new ArrayList<>();
     private ViewPager viewPager;
     private int[] imageResIds;
     private ArrayList<ImageView> imageViewList;
@@ -69,19 +58,79 @@ public class HomeFragment extends Fragment {
     String fileName = "cet4";
     private HomeViewModel homeViewModel;
 
+    //自动切换轮播图代码
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            handler.sendEmptyMessageDelayed(1,2000);
+            mViewpage.setCurrentItem(mViewpage.getCurrentItem()+1);
+            super.handleMessage(msg);
+        }
+    };
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
 
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         //test=(Button)root.findViewById(R.id.test);
+
+
         view = inflater.inflate(R.layout.fragment_home, null);
-        final TextView textView = root.findViewById(R.id.text_home);
+        //final TextView textView = root.findViewById(R.id.text_home);
         final TextView txt = root.findViewById(R.id.txt);
+        mViewpage = root.findViewById(R.id.viewpager1);
+        mLayout = root.findViewById(R.id.layout);
+        imgs.add(R.drawable.e);
+        imgs.add(R.drawable.a);
+        imgs.add(R.drawable.b);
+        imgs.add(R.drawable.c);
+        imgs.add(R.drawable.d);
+        imgs.add(R.drawable.e);
+        imgs.add(R.drawable.a);
 
-
+        for (int i=0;i<imgs.size()-2;i++){
+            ImageView imageView = new ImageView(getActivity());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(30,30);
+            params.leftMargin = 5;
+            params.rightMargin = 5;
+            imageView.setLayoutParams(params);
+            dots.add(imageView);
+            mLayout.addView(imageView);
+        }
         //textView = textView.findViewById();
+        mViewpage.setAdapter(new MyAdapter());
+        mViewpage.setCurrentItem(2);
+        setDostImgs();
+        handler.sendEmptyMessage(1);
+        mViewpage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0){
+                    mViewpage.setCurrentItem(imgs.size()-2,false);
+
+                }
+                if (position ==imgs.size()-1){
+                    mViewpage.setCurrentItem(1,false);
+
+                }
+                setDostImgs();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
         homeViewModel.getText().observe(this, new Observer<String>() {
@@ -107,7 +156,7 @@ public class HomeFragment extends Fragment {
                     private void requestDate() throws IOException {
 
 
-                        textView.setText("装修中......敬请期待");
+                        //textView.setText("装修中......敬请期待");
                         URL u = new URL("http://route.showapi.com/1211-1?showapi_appid=391443&count=1&showapi_sign=ad54ea4fa4b444e0b360c35847261ffc");
                         InputStream in = u.openStream();
                         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -185,6 +234,48 @@ public class HomeFragment extends Fragment {
         });
 
 
+    }
+
+
+    public void setDostImgs(){
+        for (int i=0 ;i<dots.size();i++){
+            if (i == mViewpage.getCurrentItem()-1){
+                dots.get(i).setBackgroundResource(R.drawable.yes);
+            }else {
+                dots.get(i).setBackgroundResource(R.drawable.no);
+            }
+        }
+    }
+
+
+    //csdn轮播图
+    public class MyAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return imgs.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            //super.destroyItem(container, position, object);
+            container.removeView((View) object);
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            //return super.instantiateItem(container, position);
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(imgs.get(position));
+            container.addView(imageView);
+            return imageView;
+        }
     }
 
 
